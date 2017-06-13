5b9c71c76a2d2058e29e6a0b23d9e8d3f8d90f9b
Tawmy <kersting.tom@gmail.com>
2017-06-13 16:18:21 +0200

    Initial commit
    Very, very rough wip, obviously
    
    Just done for the day and gotta transfer these files

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+	var zindex = 0;
+	var margin = 0;
+    
+    $( ".window" ).draggable({
+    	containment: "parent", handle: ".window-header",
+    	start: function() {
+        	if(margin > 0){
+        		margin--;
+        	}
+      	},
+    });
+    
+    $( ".window" ).resizable({containment: "parent", minHeight: 200, minWidth: 200, handles: 'n, e, s, w'});
+
+    $(".window").mousedown(function(){
+    	zindex++;
+    	$(this).css({"z-index" : zindex});
+    });
+
+    $(".button-close").click(function(){
+    	$(this).parent(".window-header").parent(".window").css({"display" : "none"});
+
+    	var item = $(this).parent(".window-header").parent(".window").attr('id');
+    	item = item.slice(0, -2);
+    	$("#" + item).removeClass("active show-indicator");
+
+    	if(margin > 0){
+        	margin--;
+        }
+    });
+
+    /* DOCK */
+
+	$(".dock-item").click(function(){
+		
+		var item = $(this);
+		
+		if(!($(this).hasClass("bounce"))){
+			setTimeout(function(){
+				$(item).removeClass("bounce");
+				$(item).addClass("show-indicator");
+				$(item).children(".full-name").removeClass("bounce-down");
+			}, 1500);
+		}
+
+		if(!($(this).hasClass("show-indicator"))){
+			$(this).addClass("bounce");
+			$(this).children(".full-name").addClass("bounce-down");
+		}
+
+		var id = $(this).attr('id');
+		id += "-w";
+		if(!($(this).hasClass("active"))){
+			margin++;
+			zindex++;
+			$("#" + id).css({"display" : "block", "left" : margin*40, "top" : margin*40, "z-index" : zindex});
+			$(this).addClass("active");
+		}
+	})
+});
