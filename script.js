b15618d8a31484b237108c62b604437d918ed98d
Tawmy <kersting.tom@gmail.com>
2017-06-14 13:47:21 +0200

    Minimizing groundwork
    +Minimizing groundwork
    ->Remember window positions of windows when minimizing
    ~Fix window content causing problems when dragging
    ~Fix dock indicator appearing despite window being already closed

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ $(document).ready(function(){
 
     var zindex = 0;
     var margin = 0;
+    var dockmitems = 0;
+    var dockwidth = 0;
+    var dockposition = 0;
+    var windowpositions = [];
+    var minimizednames = [];
+
+    $("body").mousedown(function(){
+        $(".window").css({"transition" : "0ms"});
+    });
+    $("body").mouseup(function(){
+        $(".window").css({"transition" : "600ms"});
+    });
     
     $( ".window" ).draggable({
         containment: "parent", handle: ".window-header",
@@ -9,6 +21,10 @@ $(document).ready(function(){
             if(margin > 0){
                 margin--;
             }
+            $(".window").css({"pointer-events" : "none"});
+        },
+        stop: function() {
+            $(".window").css({"pointer-events" : "initial"});
         }
     });
     
@@ -21,6 +37,7 @@ $(document).ready(function(){
 
     $(".button-close").click(function(){
         $(this).parent(".window-header").parent(".window").css({"display" : "none"});
+        $(this).parent(".window-header").parent(".window").removeClass("active show-indicator");
 
         var item = $(this).parent(".window-header").parent(".window").attr('id');
         item = item.slice(0, -2);
@@ -30,18 +47,51 @@ $(document).ready(function(){
             margin--;
         }
     });
+    $(".button-minimize").click(function(){
+        dockmitems++;
+        dockwidth = $(".dock").width();
+        $(".dock").css({"padding-right" : 95*dockmitems});
+        $(this).parent(".window-header").parent(".window").addClass("window-minimized fixed-size");
+        $(this).parent(".window-header").parent(".window").css({"left" : dockwidth/2-130+90*dockmitems});
+        var item = $(this).parent(".window-header").parent(".window").position();
+        item = item.left;
+        windowpositions.push(item);
+        item = $(this).parent(".window-header").parent(".window").attr('id');
+        minimizednames.push(item);
+    });
+    $(".window").mouseup(function(){
+        if($(this).hasClass("window-minimized")){
+
+            if(dockmitems > 0){
+                dockmitems--;
+            }
+            $(".dock").css({"padding-right" : 95*dockmitems+5});
+            $(this).removeClass("window-minimized fixed-size");
+
+            var windowname = $(this).attr('id');
+            windowname = jQuery.inArray( windowname, minimizednames);
+            item = windowpositions[windowname];
+            $(this).css({"left" : item});
+            minimizednames.splice(windowname, 1);
+            windowpositions.splice(windowname, 1);
+        }
+    });
 
     /* DOCK */
 
     $(".dock-item").click(function(){
         
         var item = $(this);
+        var id = $(this).attr('id');
+        id += "-w";
         
         if(!($(this).hasClass("bounce"))){
             setTimeout(function(){
                 $(item).removeClass("bounce");
-                $(item).addClass("show-indicator");
                 $(item).children(".full-name").removeClass("bounce-down");
+                if($("#" + id).hasClass("active")){
+                    $(item).addClass("show-indicator");
+                }
             }, 1500);
         }
 
@@ -49,9 +99,6 @@ $(document).ready(function(){
             $(this).addClass("bounce");
             $(this).children(".full-name").addClass("bounce-down");
         }
-
-        var id = $(this).attr('id');
-        id += "-w";
         
         if(!($(this).hasClass("active"))){
             
@@ -65,6 +112,7 @@ $(document).ready(function(){
             }
             
             $(this).addClass("active no-virgin");
+            $("#" + id).addClass("active");
         }
     })
 });
