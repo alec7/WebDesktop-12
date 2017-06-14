a9ca46dc5f97efb3a230000b4d13d4f86f6aebe2
Tawmy <kersting.tom@gmail.com>
2017-06-14 14:13:48 +0200

    Code comments and minor cleanup
    Figured to do this before the JS code becomes an incomprehensible mess.

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ $(document).ready(function(){
     var margin = 0;
     var dockmitems = 0;
     var dockwidth = 0;
-    var dockposition = 0;
     var windowpositions = [];
     var minimizednames = [];
+
+    /* Makes resizing and dragging not lag */
 
     $("body").mousedown(function(){
         $(".window").css({"transition" : "0ms"});
@@ -15,6 +16,8 @@ $(document).ready(function(){
         $(".window").css({"transition" : "600ms"});
     });
     
+    /* jQuery UI Draggable and Resizable functions */
+
     $( ".window" ).draggable({
         containment: "parent", handle: ".window-header",
         start: function() {
@@ -30,10 +33,17 @@ $(document).ready(function(){
     
     $( ".window" ).not(".fixed-size").resizable({containment: "parent", minHeight: 200, minWidth: 200, handles: 'n, e, s, w'});
 
+    /* Bring clicked window to foreground */
+
     $(".window").mousedown(function(){
         zindex++;
         $(this).css({"z-index" : zindex});
     });
+
+    /* Closes window
+        - Also removes indicators from corresponding dock item
+        - Adjusts default window margin value
+    */
 
     $(".button-close").click(function(){
         $(this).parent(".window-header").parent(".window").css({"display" : "none"});
@@ -47,18 +57,36 @@ $(document).ready(function(){
             margin--;
         }
     });
+
+    /* Minimizes window
+        - Widens dock through padding
+        - Minimizes window with class and specific "left" positioning property
+        - Saves window position and window name in separate arrays
+     */
+
     $(".button-minimize").click(function(){
         dockmitems++;
         dockwidth = $(".dock").width();
         $(".dock").css({"padding-right" : 95*dockmitems});
         $(this).parent(".window-header").parent(".window").addClass("window-minimized fixed-size");
         $(this).parent(".window-header").parent(".window").css({"left" : dockwidth/2-130+90*dockmitems});
+        
         var item = $(this).parent(".window-header").parent(".window").position();
         item = item.left;
         windowpositions.push(item);
         item = $(this).parent(".window-header").parent(".window").attr('id');
         minimizednames.push(item);
     });
+
+    /* Restores window
+        - Adjusts dock padding
+        - Restores window by removing class
+        - Fetches window name and searches for its position N in window name array
+        - Grabs "left" value N from window position array
+        - Sets "left" value for window
+        - Removes name and position from array
+     */
+
     $(".window").mouseup(function(){
         if($(this).hasClass("window-minimized")){
 
@@ -78,6 +106,14 @@ $(document).ready(function(){
     });
 
     /* DOCK */
+
+    /* Opens window
+        - Only adds bounce animation when not already active
+        - Adds indicator below item after bounce ends
+        - Opens window only when not already indicated as active
+        - Sets window margin to default value, N+1 times as high depending how many windows occupy the value N (you get what I mean)
+        - Adds active indicator to dock item and window, adds no-virgin class for ^
+     */
 
     $(".dock-item").click(function(){
         
